28a05f47ec4fed87a8e56e71f51d8084cca3d0f8
Caleb Morris <caleb.morris.g@gmail.com>
2012-05-11 21:56:15 -0700

    Finished the matrix of node paths
    I need to find an algorithm that accounts for the least ammount of time.
    I will probably end up doing a a* search with the heuristic being the sum of the least costly connection of each path.
    So i need a function to:
    	check if there is a path from 1 machine to another.
    	find the least costly connection of the shortest path.
    	search for the least costly full disconect (most likely using an a* search)

--- interview_street/Matrix/main.py
+++ interview_street/Matrix/main.py
@@ -4,10 +4,24 @@ k = int(tmp[1])
 print n, k
 paths = []
 machines = []
-for x in range(n-1):
+for x in xrange(n-1):
 	paths.append(raw_input())
 	print paths
 for x in range(k):
 	machines.append(input())
 	print machines
 #input done i guess
+t = []
+matrix = []
+for x in xrange(n):
+	matrix.append([])
+	for y in xrange(n):
+		matrix[x].append(-1)
+
+for x in paths:
+	y = x.split()
+	matrix[int(y[0])][int(y[1])] = int(y[2])
+	matrix[int(y[1])][int(y[0])] = int(y[2])
+
+for z in matrix:
+	print z
